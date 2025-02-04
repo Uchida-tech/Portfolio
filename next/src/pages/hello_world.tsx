55e5f8719038394f875b57163182e8752a347d7d
Uchida-tech <spaller300@gmail.com>
2025-02-04 21:42:34 +0900

    #12 setup daisy ui

--- next/src/pages/hello_world.tsx
+++ next/src/pages/hello_world.tsx
@@ -2,10 +2,14 @@ import type { NextPage } from 'next'
 import SimpleButton from '@/components/SimpleButton'
 
 const HelloWorld: NextPage = () => {
+  const handleOnClick = () => {
+    console.log('Clicked from hello_world')
+  }
+
   return (
     <>
       <div>Hello Next.js</div>
-      <SimpleButton text={'From HelloWorld'} />
+      <SimpleButton text={'From HelloWorld'} onClick={handleOnClick} />
     </>
   )
 }
