494e6a4b48c946541038916189420814294b05da
Uchida-tech <spaller300@gmail.com>
2025-02-23 01:10:47 +0900

    #45 add Loading and error component

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -22,13 +22,41 @@ const Index: NextPage = () => {
     url,
     fetcher,
   )
-  if (error) return <div>An error has occurred.</div>
-  if (!data) return <div>Loading...</div>
+  if (error)
+    return (
+      <div className="flex items-center justify-center">
+        <div role="alert" className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>
+            現在、システムに技術的な問題が発生しています。ご不便をおかけして申し訳ありませんが、復旧までしばらくお待ちください。
+          </span>
+        </div>
+      </div>
+    )
+
+  if (!data)
+    return (
+      <div className="flex justify-center">
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    )
 
   const activeRecalls = camelcaseKeys(data.active_recalls)
 
   return (
-    <div className="bg-blue-100 min-h-screen">
+    <div className="bg-white min-h-screen">
       <div className="max-w-3xl mx-auto p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {activeRecalls.map((activeRecall) => (
