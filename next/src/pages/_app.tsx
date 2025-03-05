534e16c670891bbc86ce74ec3c73e1c98c389325
Uchida-tech <spaller300@gmail.com>
2025-03-06 03:07:56 +0900

    #53_add_snackbar

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import CurrentUserFetch from '@/components/CurrentUserFetch'
 import Header from '@/components/Header'
+import { NotificationProvider } from '@/components/NotificationContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <CurrentUserFetch />
-      <Header />
-      <Component {...pageProps} />
+      <NotificationProvider>
+        <CurrentUserFetch />
+        <Header />
+        <Component {...pageProps} />
+      </NotificationProvider>
     </>
   )
 }
