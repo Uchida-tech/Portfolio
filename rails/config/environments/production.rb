2c28eba3e105bfe6f37fdcc8b1d941723cd081e0
Uchida-tech <spaller300@gmail.com>
2025-06-22 22:48:17 +0900

    fix production.rb

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -1,17 +1,19 @@
-config.action_mailer.raise_delivery_errors = true
+Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = true
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address: ENV.fetch("SMTP_ADDRESS", nil),
-  port: ENV.fetch("SMTP_PORT", nil),
-  domain: "onrender.com",
-  user_name: ENV.fetch("SENDGRID_USERNAME", nil),
-  password: ENV.fetch("SENDGRID_PASSWORD", nil),
-  authentication: :plain,
-  enable_starttls_auto: true,
-}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch("SMTP_ADDRESS", nil),
+    port: ENV.fetch("SMTP_PORT", nil),
+    domain: "onrender.com",
+    user_name: ENV.fetch("SENDGRID_USERNAME", nil),
+    password: ENV.fetch("SENDGRID_PASSWORD", nil),
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
 
-config.action_mailer.default_url_options = {
-  host: "https://myportfolio-tdl6.onrender.com",
-  protocol: "https",
-}
+  config.action_mailer.default_url_options = {
+    host: "https://myportfolio-tdl6.onrender.com",
+    protocol: "https",
+  }
+end
