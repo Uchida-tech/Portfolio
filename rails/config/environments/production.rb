d41cc39b0b031d0a6e34b982a05b9aa7eba9b0a5
Uchida-tech <spaller300@gmail.com>
2025-06-22 23:23:01 +0900

    fix production.rb with rubocop

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
+  config.eager_load = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
