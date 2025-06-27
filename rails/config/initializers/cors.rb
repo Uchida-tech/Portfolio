4b6036ceb9ba5f06fe51e00e31c32c8b3152ccd1
Uchida-tech <spaller300@gmail.com>
2025-06-27 23:24:02 +0900

    #86 add README.md

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://my-portfolio-five-lovat-o9u46t7cag.vercel.app"
+    origins 'https://my-portfolio-five-lovat-o9u46t7cag.vercel.app'
 
     resource "*",
              headers: :any,
